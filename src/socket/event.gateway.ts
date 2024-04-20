51e1ab9385111e8937860989659c4f4c82fe3874
小白 <2528358258@qq.com>
2024-04-20 22:58:12 +0800

    写注释

--- src/socket/event.gateway.ts
+++ src/socket/event.gateway.ts
@@ -12,14 +12,19 @@ import { Socket, Server } from 'socket.io';
 })
 export class EventGateway {
   @WebSocketServer() server: Server;
+  // 历史人数
+  historyCount: number = 0;
   // 聊天记录
   messageLists: any = [];
   // 在线用户
   onlineUsers: any = [];
   // 日志
   logs: any = [];
-
-  // 客户端连接时触发
+  // 记录
+  i: number = 0;
+  /**
+   * @description 客户端连接时触发
+   */
   handleConnection(@ConnectedSocket() client: Socket) {
     console.log(`用户 连接: ${client.id}`);
 
@@ -28,9 +33,13 @@ export class EventGateway {
       messageLists: this.messageLists,
       onlineUsers: this.onlineUsers,
       logs: this.logs,
+      historyCount: this.historyCount,
     });
   }
-  // 客户端断开连接时触发
+
+  /**
+   * @description 客户端断开连接时触发
+   */
   handleDisconnect(@ConnectedSocket() client: Socket) {
     // 移除在线用户
     this.onlineUsers = this.onlineUsers.filter((item: any) => {
@@ -41,11 +50,21 @@ export class EventGateway {
     // 通知其他用户
     this.server.emit('onlineUsers', this.onlineUsers);
     this.server.emit('logs', this.logs);
+    this.server.emit('historyCount', this.historyCount);
   }
 
-  // 监听客户端的消息
+  /**
+   * @description 监听客户端的消息事件
+   */
   @SubscribeMessage('newMessage')
   handleMessage(@MessageBody() body: any) {
+    if (this.messageLists.length > 200) {
+      this.i += 1;
+      this.messageLists = [];
+      this.logs.push(
+        `${new Date().toLocaleString()} 聊天记录已清空${this.i}次`,
+      );
+    }
     this.onlineUsers.map((item: any) => {
       if (item.id === body.id) {
         this.logs.push(
@@ -59,15 +78,20 @@ export class EventGateway {
     this.server.emit('newMessage', this.messageLists);
     this.server.emit('onlineUsers', this.onlineUsers);
     this.server.emit('logs', this.logs);
+    this.server.emit('historyCount', this.historyCount);
   }
-  // 监听客户端的在线用户
+
+  /**
+   * @description 监听客户端的登录事件
+   */
   @SubscribeMessage('onlineUsers')
   handleMessage2(@MessageBody() body: any) {
     this.onlineUsers.push(body);
     this.logs.push(`${new Date().toLocaleString()} ${body.name} 加入聊天室`);
-
+    this.historyCount += 1;
     // 通知其他用户
     this.server.emit('onlineUsers', this.onlineUsers);
     this.server.emit('logs', this.logs);
+    this.server.emit('historyCount', this.historyCount);
   }
 }
