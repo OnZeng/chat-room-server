4b9f79fa733070e61039c0ffa97c56fe8004c891
小白 <2528358258@qq.com>
2024-04-20 19:20:20 +0800

    优化

--- dist-client/socket/event.gateway.d.ts
+++
@@ -1,7 +0,0 @@
-import { Socket, Server } from 'socket.io';
-export declare class EventGateway {
-    server: Server;
-    list: any;
-    handleConnection(client: Socket): void;
-    handleMessage(body: any): void;
-}
