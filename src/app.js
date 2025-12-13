82bd9d94ce6a991741babab4edb3fb8bde59cf56
ming <2528358258@qq.com>
2025-12-14 02:50:27 +0800

    feat: 添加历史API回退中间件并更新前端资源
    添加koa2-connect-history-api-fallback中间件以支持前端路由
    更新前端资源文件包括HTML、CSS和JS
    重构部分前端代码结构

--- src/app.js
+++ src/app.js
@@ -1,43 +1,58 @@
-import koa from "koa";
+import koa from 'koa';
 import { koaBody } from 'koa-body';
-import koaStatic from 'koa-static'
-import cors from "@koa/cors";
-import koaHelmet from "koa-helmet";
+import koaStatic from 'koa-static';
+import cors from '@koa/cors';
+import koaHelmet from 'koa-helmet';
 import http from 'http';
 import { Server } from 'socket.io';
-import { prRouter, puRouter } from "./routes/index.js";
-import { initJsonDB } from "./mw/index.js";
-import { bootstrap } from "./socket/index.js";
+import { historyApiFallback } from 'koa2-connect-history-api-fallback';
+import { prRouter, puRouter } from './routes/index.js';
+import { initJsonDB } from './mw/index.js';
+import { bootstrap } from './socket/index.js';
 
-console.log("----------------------启动中----------------------");
+console.log('----------------------启动中----------------------');
 
 if (process.env.SOCKET_ENABLED === 'true') {
-    const app = new koa();
-    app.use(koaStatic('dist'))
-    // 初始化websocket服务
-    console.log("初始化websocket服务");
-    const server = http.createServer(app.callback());
-    const io = new Server(server);
-    //初始化数据库
-    const allDB = await initJsonDB()
-    bootstrap(io, allDB);
-    server.listen(process.env.SOCKET_PORT, () => {
-        console.log("websocket服务启动成功，访问地址:http://127.0.0.1:" + process.env.SOCKET_PORT || 3000);
-    });
+  const app = new koa();
+  app.use(historyApiFallback());
+  app.use(koaStatic('dist'));
+  // 初始化websocket服务
+  console.log('初始化websocket服务');
+  const server = http.createServer(app.callback());
+  const io = new Server(server);
+  //初始化数据库
+  const allDB = await initJsonDB();
+  bootstrap(io, allDB);
+  server.listen(process.env.SOCKET_PORT, () => {
+    console.log(
+      'websocket服务启动成功，访问地址:http://127.0.0.1:' +
+        process.env.SOCKET_PORT || 3000
+    );
+  });
 } else {
-    const app = new koa();
+  const app = new koa();
+  app.use(historyApiFallback());
+  //初始化数据库
+  app.context.allDB = await initJsonDB();
 
-    //初始化数据库
-    app.context.allDB = await initJsonDB()
+  app.use(
+    koaBody({
+      patchNode: true,
+      multipart: false,
+      json: false,
+      jsonStrict: true,
+    })
+  ); // 解析请求体
+  app.use(koaHelmet()); // 安全相关中间件
+  app.use(cors()); // 允许跨域
+  app.use(prRouter.routes());
+  app.use(puRouter.routes());
 
-    app.use(koaBody({ patchNode: true, multipart: false, json: false, jsonStrict: true })); // 解析请求体
-    app.use(koaHelmet()); // 安全相关中间件
-    app.use(cors()); // 允许跨域
-    app.use(prRouter.routes());
-    app.use(puRouter.routes());
-
-    app.listen(process.env.HTTP_PORT || 3000, () => {
-        // console.log(process.env.TOKEN_SECRET);
-        console.log("http服务启动成功，访问地址:http://127.0.0.1:" + process.env.HTTP_PORT || 3000);
-    });
+  app.listen(process.env.HTTP_PORT || 3000, () => {
+    // console.log(process.env.TOKEN_SECRET);
+    console.log(
+      'http服务启动成功，访问地址:http://127.0.0.1:' + process.env.HTTP_PORT ||
+        3000
+    );
+  });
 }
